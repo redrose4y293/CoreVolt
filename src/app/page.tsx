9bec26180dfda29ad70cc504d781ea71c5b26651
Ehsan Nawaz <bc240412347ena@vu.edu.pk>
2025-10-15 15:05:37 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
           <div className="relative w-full rounded-xl overflow-hidden bg-black/5 animate-fade-in-right flex items-center justify-center">
             <div className="relative w-full h-64 md:h-96">
               <Image
-                src="/earth's-internal-power.png"
+                src="public/earth's-internal-power.png"
                 alt="Earth internal power"
                 fill
                 style={{ objectFit: "contain" }}
